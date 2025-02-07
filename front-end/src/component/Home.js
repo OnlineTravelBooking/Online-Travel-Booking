56d3f57f57a4aeea0833fb1d0695f6182f665ce9
Monai Suchatanon <6710110343@psu.ac.th>
2025-02-07 20:02:40 +0700

    header เปลี่ยนเมื่อ login แล้ว

--- front-end/src/component/Home.js
+++ front-end/src/component/Home.js
@@ -1,9 +1,36 @@
 import React from "react";
-
+import { UserHeader, LoggedIn } from "./Header";
+import { Layout, Menu } from "antd";
+import { useAuth } from "../AuthContext";
+const { Header } = Layout;
 export default function Home() {
-  return (
-    <div>
-      <h1>Home yay!</h1>
-    </div>
-  );
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <Layout>
+          <Header>
+            <UserHeader />
+          </Header>
+        </Layout>
+        <div>
+          <h1>Home YAY!</h1>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Layout>
+          <Header>
+            <LoggedIn />
+          </Header>
+        </Layout>
+        <div>
+          <h1>Home YAY!</h1>
+        </div>
+      </div>
+    );
+  }
 }
