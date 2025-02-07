0b65348ec980ef407368b40f0b581f38c4c1be9d
6710110276@psu.ac.th <6710110276@psu.ac.th>
2025-02-07 20:07:20 +0700

    เพิ่ม css

--- front-end/src/component/LoginScreen.js
+++ front-end/src/component/LoginScreen.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useAuth } from "../AuthContext";
 import { useNavigate } from "react-router-dom";
 import { MUTATION_LOGIN, ROLE } from "../Graphql";
-import { Button, Checkbox, Form, Input, message } from "antd";
+import { Button, Checkbox, Form, Input, message, Layout } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { useMutation, useLazyQuery } from "@apollo/client";
 
 export default function LoginScreen() {
@@ -64,52 +65,89 @@ export default function LoginScreen() {
     }
   }, [data_role, navigate]);
 
+  const { Content } = Layout;
+
+  const contentStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "calc(100vh - 70px)",
+    backgroundImage:
+      "url('https://scontent.fbkk9-3.fna.fbcdn.net/v/t39.30808-6/297825257_5731344180211280_6680882033599668400_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=vSj_xU3_r0UQ7kNvgH-jnlS&_nc_oc=AdjTTmV9WdLAFzBpcHDLWc3Sl-m4zT8IEtEBruSMCEd4DhQ20KY9CyMzHA5CFI2b50M&_nc_zt=23&_nc_ht=scontent.fbkk9-3.fna&_nc_gid=AzRm1E9yElllBt5tmo4cyJ-&oh=00_AYC-Q__4mJxJD1zBbpvO5svsM4_ATdG0uRFrKIy_RmMfHw&oe=67ABBCA4')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
-    <Form
-      form={form}
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={handleSubmit}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your username!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item name="remember" valuePropName="checked" label={null}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item label={null}>
-        <Button type="primary" htmlType="submit" loading={loading}>
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+    <Layout style={{ minHeight: "100vh", overflow: "hidden" }}>
+      <Content style={contentStyle}>
+        <div
+          style={{
+            margin: "0 auto",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Form
+            className="formLogin"
+            form={form}
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={handleSubmit}
+            autoComplete="off"
+          >
+            <p>Login</p>
+            <p>sign in to your account</p>
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input
+                className="Inputusername"
+                placeholder="Username"
+                prefix={<UserOutlined />}
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password
+                className="Inputpassword"
+                placeholder="Password"
+                prefix={<LockOutlined />}
+                type="password"
+              />
+            </Form.Item>
+            <Form.Item label={null}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                loading={loading}
+                style={{ width: "100%" }}
+              >
+                Login
+              </Button>
+            </Form.Item>
+            <Form.Item name="remember" valuePropName="checked" label={null}>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+          </Form>
+        </div>
+      </Content>
+    </Layout>
   );
 }
