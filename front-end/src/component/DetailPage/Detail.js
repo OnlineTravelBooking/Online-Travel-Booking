54243cbb3afad005b0d49f99fa8a3185148063b0
Monai15 <94960932+Monai15@users.noreply.github.com>
2025-02-14 01:11:54 +0700

    Demo1 (#59)
    * Feat/detail page (#53)
    
    * Add: ปรับ UI detail page
    
    * Refactor Detail and Transaction components for improved layout and styling
    
    * Enhance Detail page layout and styling with responsive design elements
    
    * RESET
    
    * Enhance DetailPage and PackageCard components with improved styling and layout adjustments
    
    * RESET
    
    * header มีปุ่ม home + qr code (ปลอม) หน้า transaction (#55)
    
    * add: โชว์ข้อมูลลูกค้าหน้า transaction
    
    * add: เพิ่มสถานที่นัดรับ
    
    * fix: จองได้โดยไม่เลือกวันที่
    
    * Feature/user menu (#36)
    
    * feat: add user menu with logout functionality in Header component
    
    * feat: implement logout functionality in AuthContext and update Header component
    
    * feat: update Header component with new menu items and localization for login and logout
    
    * navbar แสดง username ของผู้ใช้ได้แล้ว
    
    ---------
    
    Co-authored-by: Thanwa.C <6410110664@email.psu.ac.th>
    
    * delete: ลบ header ไม่จำเป็นออก
    
    * add: เพิ่มปุ่ม upload
    
    * add: กดจองได้สำเร็จ
    
    * refactor: improve transaction page UI and message handling
    
    * add: แสดงหน้าสถานะการจอง
    
    * fix: downgrade React and ReactDOM to version 18.2.0; update StatusPage and Transaction components for improved functionality
    
    * add: ดึงรูปแพ๊คเกจได้
    
    * update: modify database file with new data
    
    * delete: console.log
    
    * add: header return home ได้ หน้า status เพิ่มรูป qr code
    
    ---------
    
    Co-authored-by: Monai Suchatanon <6710110343@psu.ac.th>
    Co-authored-by: Thanwa.C <6410110664@email.psu.ac.th>
    Co-authored-by: Pacharapol Piyadilok <6710110276@psu.ac.th>
    
    * Update Transaction page and Detail page (UI) (#57)
    
    * Add: ปรับ UI detail page
    
    * Refactor Detail and Transaction components for improved layout and styling
    
    * Enhance Detail page layout and styling with responsive design elements
    
    * RESET
    
    * Enhance DetailPage and PackageCard components with improved styling and layout adjustments
    
    * RESET
    
    * Fix date selection handling in Detail component
    
    * Enhance Detail and Transaction components with improved layout and formatting
    
    * Remove duplicate form field value setting in Detail component
    
    ---------
    
    Co-authored-by: Pacharapol Piyadilok <6710110276@psu.ac.th>
    Co-authored-by: Monai Suchatanon <6710110343@psu.ac.th>
    Co-authored-by: Thanwa.C <6410110664@email.psu.ac.th>

--- front-end/src/component/DetailPage/Detail.js
+++ front-end/src/component/DetailPage/Detail.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { UserHeader } from "../Header/UserHeader";
 import { useLocation, useNavigate } from "react-router-dom";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
-import { Button, Form, Select, message } from "antd";
+import { Button, Form, Select, message, Layout, Col, Row, Avatar } from "antd";
 import {
   PlusOutlined,
   MinusOutlined,
   ConsoleSqlOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
 import { TRAVEL_DATE, ALL_IMAGES_PACKAGE } from "../../Graphql";
 import { useQuery } from "@apollo/client";
@@ -15,6 +16,8 @@ import ImageSlider from "./ImageSlider";
 import { useAuth } from "../../AuthContext";
 const { Option } = Select;
 
+const { Content } = Layout;
+
 export default function Detail() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -22,6 +25,7 @@ export default function Detail() {
   const { data, isAuthenticated } = useAuth();
   const { documentId, Title, Price, Type, Description, MeetingPoint } =
     location.state || {};
+  const formattedType = Type.replaceAll("_", " ");
   const [totalPrice, setTotalPrice] = useState(Price);
   const [count, setCount] = useState(1);
   const [availableDates, setAvailableDates] = useState([]);
@@ -70,6 +74,7 @@ export default function Detail() {
   const handleDateChange = (key) => {
     const showDate = availableDates.find((item) => item.documentId === key);
     setSelectedDate(showDate);
+    form.setFieldsValue({ select: key });
   };
 
   useEffect(() => {
@@ -85,15 +90,15 @@ export default function Detail() {
       .then(() => {
         isAuthenticated
           ? navigate("/transaction", {
-            state: {
-              data: data,
-              Title: Title,
-              Price: totalPrice * count,
-              selectedDate: selectedDate,
-              people: count,
-              packageId: documentId,
-            },
-          })
+              state: {
+                data: data,
+                Title: Title,
+                Price: totalPrice * count,
+                selectedDate: selectedDate,
+                people: count,
+                packageId: documentId,
+              },
+            })
           : navigate("/login");
       })
       .catch((err) => {
@@ -105,71 +110,115 @@ export default function Detail() {
     message.error("กรุณาเลือกวันที่");
   };
   return (
-    <div>
+    <Layout style={{ minHeight: "100vh" }}>
       <UserHeader />
-      <div>
-        <div>{Title}</div>
+      <Content style={{ display: "flex", flexDirection: "column" }}>
+        <div className="Title-detail">
+          <div>{Title}</div>
+        </div>
         <div>
-          <div>{Type}</div>
-          <div>{MeetingPoint}</div>
           <div>
             <ImageSlider allImages={allImages} />
           </div>
-          <BlocksRenderer content={Description} />
+          <div className="Type-Trip-box">
+            <div className="Type-Trip">{formattedType}</div>
+          </div>
+          <Row>
+            <Col span={15} className="Detail">
+              <div className="Detail-Tour">รายละเอียดทริปทัวร์</div>
+              <BlocksRenderer content={Description} />
+            </Col>
+            <Col span={7} className="Detail-input">
+              <div>
+                <Form
+                  form={form}
+                  onFinish={handleSubmit}
+                  onFinishFailed={onFinishFailed}
+                >
+                  <div className="Background-add">
+                    <Avatar shape="square" size={64} icon={<UserOutlined />} />
+                    <div className="Member-Trip">จำนวนลูกค้า/ท่าน</div>
+                    <div style={{ scale: "1.2" }}>
+                      <Button
+                        className="Add-Button"
+                        type="primary"
+                        shape="circle"
+                        icon={<MinusOutlined />}
+                        onClick={() =>
+                          count > 1
+                            ? setCount((count) => count - 1)
+                            : setCount(1)
+                        }
+                      />
+                      {count}
+                      <Button
+                        className="Add-Button"
+                        type="primary"
+                        shape="circle"
+                        icon={<PlusOutlined />}
+                        onClick={() => setCount((count) => count + 1)}
+                      />
+                    </div>
+                  </div>
+                  <div className="line">
+                    _______________________________________________________
+                  </div>
+                  {/* ช่องเลือกวันที่ */}
+                  <div className="Day-Trip">
+                    <Form.Item
+                      name="select"
+                      hasFeedback
+                      rules={[
+                        {
+                          required: true,
+                          message: "กรุณาเลือกวันเที่ยว",
+                        },
+                      ]}
+                    >
+                      <div className="title-input">เลือกวันที่ต้องการจอง</div>
+                      <Select
+                        placeholder="เลือกวันเที่ยว"
+                        onChange={handleDateChange}
+                      >
+                        {availableDates?.map((date) => (
+                          <Option key={date.documentId}>
+                            {dayjs(date.Start_Date).format("DD/MM/YYYY")}
+                            {date.End_Date &&
+                              ` - ${dayjs(date.End_Date).format("DD/MM/YYYY")}`}
+                          </Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                  </div>
+                  <div className="line">
+                    _______________________________________________________
+                  </div>
+                  <div className="Meeting-box">จุดนัดพบ</div>
+                  <div className="Meeting">{MeetingPoint}</div>
+                  <div className="line">
+                    _______________________________________________________
+                  </div>
+                  <div className="title-cost">ราคาที่ต้องชำระ</div>
+                  <div className="Pay-box">
+                    <div className="Cost">
+                      THB {count === 0 ? totalPrice : totalPrice * count}
+                    </div>
+                    <div>
+                      <Button
+                        className="pay-button"
+                        type="primary"
+                        htmlType="submit"
+                      >
+                        ชำระเงิน
+                      </Button>
+                    </div>
+                  </div>
+                </Form>
+              </div>
+            </Col>
+          </Row>
         </div>
-        {/* ช่องเลือกวันที่ */}
-        <div>
-          <Form
-            form={form}
-            onFinish={handleSubmit}
-            onFinishFailed={onFinishFailed}
-          >
-            <Form.Item
-              name="select"
-              hasFeedback
-              rules={[
-                {
-                  required: true,
-                  message: "กรุณาเลือกวันเที่ยว",
-                },
-              ]}
-            >
-              <Select placeholder="เลือกวันเที่ยว" onChange={handleDateChange}>
-                {availableDates?.map((date) => (
-                  <Option key={date.documentId}>
-                    {dayjs(date.Start_Date).format("DD/MM/YYYY")}
-                    {date.End_Date &&
-                      ` - ${dayjs(date.End_Date).format("DD/MM/YYYY")}`}
-                  </Option>
-                ))}
-              </Select>
-            </Form.Item>
-            <div>{count}</div>
-            <div>
-              <Button
-                type="primary"
-                shape="circle"
-                icon={<MinusOutlined />}
-                onClick={() =>
-                  count > 1 ? setCount((count) => count - 1) : setCount(1)
-                }
-              />
-              {count === 0 ? totalPrice : totalPrice * count}
-              <Button
-                type="primary"
-                shape="circle"
-                icon={<PlusOutlined />}
-                onClick={() => setCount((count) => count + 1)}
-              />
-            </div>
-            <div>
-              <Button type="primary" htmlType="submit">
-                จองวันเที่ยว
-              </Button>
-            </div>
-          </Form>
-        </div>
-      </div>
-    </div>
+      </Content>
+    </Layout>
   );
 }
--- front-end/src/component/Header/UserHeader.js
+++ front-end/src/component/Header/UserHeader.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Menu, Layout } from "antd";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../AuthContext";
 import {
   FormOutlined,
   FileTextOutlined,
   LogoutOutlined,
   UserOutlined,
+  HomeOutlined,
 } from "@ant-design/icons";
 const { Header } = Layout;
 
 export const UserHeader = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const { isAuthenticated, logout, data } = useAuth();
   const [fullName, setFullName] = useState("User"); // ชื่อผู้ใช้
 
@@ -30,64 +32,62 @@ export const UserHeader = () => {
 
   if (!isAuthenticated) {
     return (
-      <Layout>
-        <Header>
-          <div className="container-fluid">
-            <div className="header">
-              <div className="logo" />
-              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-                <Menu.Item
-                  key="3"
-                  style={{ marginLeft: "auto" }}
-                  onClick={() => navigate("/login")}
-                >
-                  <UserOutlined /> เข้าสู่ระบบ
-                </Menu.Item>
-                <Menu.Item key="4" onClick={() => navigate("/login")}>
-                  <FormOutlined /> ลงทะเบียน
-                </Menu.Item>
-              </Menu>
-            </div>
+      <Header>
+        <div className="container-fluid">
+          <div className="header">
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+              <Menu.Item
+                key="3"
+                style={{ marginLeft: "auto" }}
+                onClick={() => navigate("/login")}
+              >
+                <UserOutlined /> เข้าสู่ระบบ
+              </Menu.Item>
+              <Menu.Item key="4" onClick={() => navigate("/login")}>
+                <FormOutlined /> ลงทะเบียน
+              </Menu.Item>
+            </Menu>
           </div>
-        </Header>
-      </Layout>
+        </div>
+      </Header>
     );
   } else {
     return (
-      <Layout>
-        <Header>
-          <div className="container-fluid">
-            <div className="header">
-              <div className="logo" />
-              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-                <Menu.Item
-                  key="3"
-                  style={{ marginLeft: "auto" }}
-                  onClick={() => navigate("/status")}
-                >
-                  <FileTextOutlined /> ตรวจสอบสถานะการจองทัวร์
-                </Menu.Item>
-                <Menu.SubMenu
-                  key="user"
-                  title={
-                    <span>
-                      <UserOutlined /> {fullName}
-                    </span>
-                  }
-                >
-                  <Menu.Item
-                    key="logout"
-                    icon={<LogoutOutlined />}
-                    onClick={logout}
-                  >
-                    ออกจากระบบ
-                  </Menu.Item>
-                </Menu.SubMenu>
-              </Menu>
-            </div>
-          </div>
-        </Header>
-      </Layout>
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item
+            key="3"
+            style={{ marginLeft: "auto" }}
+            onClick={() =>
+              navigate(location.pathname === "/status" ? "/" : "/status")
+            }
+          >
+            {location.pathname === "/status" ? (
+              <>
+                <HomeOutlined /> Home
+              </>
+            ) : (
+              <>
+                <FileTextOutlined /> ตรวจสอบสถานะการจองทัวร์
+              </>
+            )}
+          </Menu.Item>
+          <Menu.SubMenu
+            key="user"
+            title={
+              <span>
+                <UserOutlined /> {fullName}
+              </span>
+            }
+          >
+            <Menu.Item key="logout" icon={<LogoutOutlined />} onClick={logout}>
+              ออกจากระบบ
+            </Menu.Item>
+          </Menu.SubMenu>
+        </Menu>
+      </Header>
     );
   }
 };
