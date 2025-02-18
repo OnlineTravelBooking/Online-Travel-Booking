e070a73debdda82cdc6877b1cd66e7779e3a127d
Pacharapol Piyadilok <6710110276@psu.ac.th>
2025-02-19 01:22:49 +0700

    Feat/detail page (#65)
    * Add: ปรับ UI detail page
    
    * Refactor Detail and Transaction components for improved layout and styling
    
    * Enhance Detail page layout and styling with responsive design elements
    
    * RESET
    
    * Enhance DetailPage and PackageCard components with improved styling and layout adjustments
    
    * RESET
    
    * Fix date selection handling in Detail component
    
    * Enhance Detail and Transaction components with improved layout and formatting
    
    * Remove duplicate form field value setting in Detail component
    
    * Add framer-motion for animations in ImageSlider and PackageCard components; update styles in index.css
    
    * Add Bootstrap for styling and implement image carousel in Home component
    
    * Add images for carousel and enhance Slide component with search functionality and improved controls

--- front-end/src/component/HomePage/PackageCard.js
+++ front-end/src/component/HomePage/PackageCard.js
@@ -4,6 +4,8 @@ import { GET_PACKAGES } from "../../Graphql";
 import { useQuery } from "@apollo/client";
 import Title from "antd/es/skeleton/Title";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { spaceChildren } from "antd/es/button";
 const { Meta } = Card;
 
 export default function PackageCard() {
@@ -28,48 +30,66 @@ export default function PackageCard() {
 
   console.log("data:", dataSource);
   return (
-    <Row gutter={[16, 16]} style={{ padding: "24px" }}>
-      {dataSource.map((item) => (
+    <Row className="Package">
+      {dataSource.map((item, index) => (
         <Col xs={24} sm={12} md={8} lg={6} key={item.documentId}>
-          <Card
-            hoverable
-            style={{
-              width: "100%",
-              height: "100%",
+          <motion.div
+            initial={{ x: 100, opacity: 0, rotate: 0 }}
+            animate={{ x: 0, opacity: 1, rotate: 0 }}
+            transition={{
+              delay: index * 0.2,
+              duration: 0.5,
             }}
-            cover={
-              <img
-                alt={item.Title}
-                src={`http://localhost:1337${item.urlImage}`}
-                style={{
-                  height: "200px",
-                  objectFit: "cover",
-                }}
-              />
-            }
-            onClick={() => {
-              navigate("/detail", {
-                state: {
-                  documentId: item.documentId,
-                  Title: item.Title,
-                  Price: item.Price,
-                  Type: item.Type,
-                  Description: item.Description,
-                  MeetingPoint: item.MeetingPoint,
-                },
-              });
+            whileHover={{
+              scale: 1.05,
+              x: [0, -10, 10, 0],
+              rotate: [0, -1, 1, 0],
+              transition: {
+                duration: 0.5,
+              },
+            }}
+            whileTap={{
+              scale: 0.9,
+              transition: { type: "spring", stiffness: 1000 },
             }}
           >
-            <Meta
-              title={item.Title}
-              description={
-                <>
-                  <div>{item.Type}</div>
-                  <div style={{ color: "#FF0000" }}>${item.Price}</div>
-                </>
+            <Card
+              hoverable
+              className="Card-package"
+              cover={
+                <img
+                  alt={item.Title}
+                  src={`http://localhost:1337${item.urlImage}`}
+                  style={{
+                    height: "170px",
+                    objectFit: "contain",
+                  }}
+                />
               }
-            />
-          </Card>
+              onClick={() => {
+                navigate("/detail", {
+                  state: {
+                    documentId: item.documentId,
+                    Title: item.Title,
+                    Price: item.Price,
+                    Type: item.Type,
+                    Description: item.Description,
+                    MeetingPoint: item.MeetingPoint,
+                  },
+                });
+              }}
+            >
+              <Meta
+                title={item.Title}
+                description={
+                  <>
+                    <div>{item.Type}</div>
+                    <div style={{ color: "#FF0000" }}>${item.Price}</div>
+                  </>
+                }
+              />
+            </Card>
+          </motion.div>
         </Col>
       ))}
     </Row>
