c2e2fdd6ed4abafea3d60b31978bd28d55e0df67
6710110276@psu.ac.th <6710110276@psu.ac.th>
2025-02-21 16:37:11 +0700

    Enhance filter component with form inputs and styling adjustments

--- front-end/src/component/HomePage/Filter.js
+++ front-end/src/component/HomePage/Filter.js
@@ -10,9 +10,9 @@ import {
   Segmented,
   Select,
   TreeSelect,
+  Radio,
   Layout,
 } from "antd";
-
 import { FilterOutlined } from "@ant-design/icons";
 
 const { Sider } = Layout;
@@ -20,5 +20,33 @@ const { RangePicker } = DatePicker;
 const { Option } = Select;
 
 export default function SiderFilter() {
-  return <div className="Filter-Box">James</div>;
+  const [form] = Form.useForm();
+  const variant = Form.useWatch("variant", form);
+  return (
+    <div className="Filter-Box">
+      <Form
+        form={form}
+        variant={variant || "filled"}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          variant: "filled",
+        }}
+        className="Filter-form"
+      >
+        <Form.Item label="Input" name="Input">
+          <Input />
+        </Form.Item>
+        <Radio>One day trip</Radio>
+        <Radio>Muti day trip</Radio>
+        <Form.Item label="DatePicker" name="DatePicker">
+          <DatePicker />
+        </Form.Item>
+        <Form.Item label="RangePicker" name="RangePicker">
+          <RangePicker />
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
