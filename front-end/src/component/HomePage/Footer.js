f50c461534ddb72aec8258be5353b34274d7a6d9
ziczac <tanatan@Vindice.local>
2025-02-27 16:25:50 +0700

    เพิ่มข้อความที่footer และเพิ่มลิ้งก์เฟส,ไอจี

--- front-end/src/component/HomePage/Footer.js
+++ front-end/src/component/HomePage/Footer.js
@@ -1,14 +1,35 @@
 import React from "react";
 import { Layout } from "antd";
+import { FacebookOutlined, InstagramOutlined } from "@ant-design/icons";
 
 const { Footer } = Layout;
 
 const footerStyle = {
-  textAlign: "center",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
   color: "#fff",
-  backgroundColor: "#4096ff",
+  backgroundColor: "#2E5077",
+  padding: "10px 20px",
+  fontSize: "20px",
 };
 
+const iconStyle = { fontSize: "25px", margin: "0 8px", color: "#fff" };
+
 export default function CustomFooter() {
-  return <Footer style={footerStyle}>Footer</Footer>;
+  return (
+    <Footer style={footerStyle}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <span style={{ marginRight: "15px" }}>©SOUTHEX</span>
+        <span>Follow : Facebook</span>
+        <a href="https://facebook.com">
+          <FacebookOutlined style={iconStyle} />
+        </a>
+        <span>Instagram</span>
+        <a href="https://instagram.com">
+          <InstagramOutlined style={iconStyle} />
+        </a>
+      </div>
+    </Footer>
+  );
 }
